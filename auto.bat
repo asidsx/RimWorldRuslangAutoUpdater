8a2c9d5b03f6443043023d09664cf369e4188380
asidsx <106923482+asidsx@users.noreply.github.com>
2022-07-21 22:10:49 +0500

    Add files via upload

--- auto.bat
+++ auto.bat
@@ -2,7 +2,7 @@ powershell.exe -Command "Invoke-WebRequest -OutFile ./master.zip https://github.
 powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem';[IO.Compression.ZipFile]::ExtractToDirectory('master.zip', '1');"
 RD /s /q "Data\Core\Languages\Russian (Русский)\"
 RD /s /q "Data\Royalty\Languages\Russian (Русский)\"
-RD /s /q "Data\Ideology\Languages\Russian (Русский)\
+RD /s /q "Data\Ideology\Languages\Russian (Русский)\"
 del "Data\Royalty\Languages\Russian (Русский).tar"
 del "Data\Core\Languages\Russian (Русский).tar"
 del "Data\Ideology\Languages\Russian (Русский).tar"

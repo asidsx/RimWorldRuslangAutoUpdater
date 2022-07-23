26f756f2bdf62827f1f5b2af29694e6f38efafe9
Andrey Lemin <anlemin@mail.ru>
2022-07-23 16:18:29 +0300

    Use latin symbols. Use variables for temp path and localization name. Don't delete default Steam localization

--- auto.bat
+++ auto.bat
@@ -1,13 +1,12 @@
-powershell.exe -Command "Invoke-WebRequest -OutFile ./master.zip https://github.com/Ludeon/RimWorld-ru/archive/master.zip
-powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem';[IO.Compression.ZipFile]::ExtractToDirectory('master.zip', '1');"
-RD /s /q "Data\Core\Languages\Russian (Русский)\"
-RD /s /q "Data\Royalty\Languages\Russian (Русский)\"
-RD /s /q "Data\Ideology\Languages\Russian (Русский)\"
-del "Data\Royalty\Languages\Russian (Русский).tar"
-del "Data\Core\Languages\Russian (Русский).tar"
-del "Data\Ideology\Languages\Russian (Русский).tar"
-xcopy "1\RimWorld-ru-master\Core" "Data\Core\Languages\Russian (Русский)\"  /H /Y /C /R /S
-xcopy "1\RimWorld-ru-master\Royalty" "Data\Royalty\Languages\Russian (Русский)\"  /H /Y /C /R /S
-xcopy "1\RimWorld-ru-master\Ideology" "Data\Ideology\Languages\Russian (Русский)\"  /H /Y /C /R /S
-RD /s /q 1
+set tempDirName=temp
+set localizationName=Russian (GitHub)
+powershell.exe -Command "Invoke-WebRequest -OutFile ./master.zip https://github.com/Ludeon/RimWorld-ru/archive/master.zip"
+powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem';[IO.Compression.ZipFile]::ExtractToDirectory('master.zip', '%tempDirName%');"
+RD /s /q "Data\Core\Languages\%localizationName%\"
+RD /s /q "Data\Royalty\Languages\%localizationName%\"
+RD /s /q "Data\Ideology\Languages\%localizationName%\"
+xcopy "%tempDirName%\RimWorld-ru-master\Core" "Data\Core\Languages\%localizationName%\"  /H /Y /C /R /S
+xcopy "%tempDirName%\RimWorld-ru-master\Royalty" "Data\Royalty\Languages\%localizationName%\"  /H /Y /C /R /S
+xcopy "%tempDirName%\RimWorld-ru-master\Ideology" "Data\Ideology\Languages\%localizationName%\"  /H /Y /C /R /S
+RD /s /q %tempDirName%
 del master.zip

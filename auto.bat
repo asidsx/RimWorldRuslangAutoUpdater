08f74ea19c026fd59b1e223995256c0fb1d11c44
Max <57110948+kamadz@users.noreply.github.com>
2022-11-15 20:48:28 +0400

    Update auto.bat
    Add Biotech baths

--- auto.bat
+++ auto.bat
@@ -5,8 +5,10 @@ powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem';[IO.Compression.Z
 RD /s /q "Data\Core\Languages\%localizationName%\"
 RD /s /q "Data\Royalty\Languages\%localizationName%\"
 RD /s /q "Data\Ideology\Languages\%localizationName%\"
+RD /s /q "Data\Biotech\Languages\%localizationName%\"
 xcopy "%tempDirName%\RimWorld-ru-master\Core" "Data\Core\Languages\%localizationName%\"  /H /Y /C /R /S
 xcopy "%tempDirName%\RimWorld-ru-master\Royalty" "Data\Royalty\Languages\%localizationName%\"  /H /Y /C /R /S
 xcopy "%tempDirName%\RimWorld-ru-master\Ideology" "Data\Ideology\Languages\%localizationName%\"  /H /Y /C /R /S
+xcopy "%tempDirName%\RimWorld-ru-master\Biotech" "Data\Biotech\Languages\%localizationName%\"  /H /Y /C /R /S
 RD /s /q %tempDirName%
 del master.zip
